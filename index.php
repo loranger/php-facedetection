e5f5bb349bfec94b0a3fcfdc33a9537770b22d26
loranger <loranger@free.fr>
2012-06-06 11:58:54 +0200

    [Upg] Rewrite using imagick instead of GD, remove all notices (can now be used with error_reporting(E_ALL)), new methods

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
+set_time_limit(0);
+
 include "FaceDetector.php";
 
-$detector = new Face_Detector('detection.dat');
-$detector->face_detect('lena512color.jpg');
-$detector->toJpeg();
+$detector = new FaceDetector('lena512color.jpg'); // Load an image and detect faces
+
+//$detector->setPadding(50); /// Define a padding or use the default relative one
+
+//$detector->crop(); // Crop face
+
+$detector->highlight(); // Or highlight face
+
+//$detector->save(); // Save the current modification in the current file (highlight or crop)
+//$detector->save('/home/user/export.png'); // Or save in another current file
+
+$detector->display(); // Or display file as jpg
+//$detector->display('png'); // Or as png
+die();
